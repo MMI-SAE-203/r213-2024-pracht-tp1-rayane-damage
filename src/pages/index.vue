f778af0426d90be36833b55fff26c7e86c30d05a
rayane-damage <150058299+rayane-damage@users.noreply.github.com>
2024-04-12 10:29:45 +0200

    accordeon html

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,4 +4,16 @@
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
+  <section>
+    <button class="text-xl">bouton 1</button>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius natus nesciunt provident odit tempore dicta praesentium eos, ad quidem iusto magni. Odit, ipsum recusandae. Iusto, cumque sed. Debitis, cum nihil?</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 2</button>
+    <p>Aliquid sit non maiores exercitationem nemo, fugit accusantium autem temporibus in consequatur ullam! Nesciunt aliquid, aut temporibus doloremque consectetur nam illo nulla! Consectetur porro sequi mollitia ad ratione? Vel, nisi!</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 3</button>
+    <p>Itaque laborum aliquid perferendis sequi nesciunt eum assumenda ex magni quos tempora, vitae corrupti adipisci, incidunt expedita reiciendis, mollitia rem exercitationem odit corporis! Praesentium dolore perferendis iure, id iste totam!</p>
+  </section>
 </template>
